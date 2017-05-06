2777e57e5a493fc5bfe3279b54596fc7e2d48a8b
Natashia Copple <natashiacopple@gmail.com>
2017-05-06 00:52:47 -0700

    Add files via upload

---
+++ RedBlueYellow.java
@@ -0,0 +1,11 @@
+
+public class RedBlueYellow
+{
+    public static void main(String[] args) 
+    {
+         //E1.11
+        System.out.print("Red\n");
+        System.out.print("Blue\n");
+        System.out.print("Yellow\n");
+    }
+}
